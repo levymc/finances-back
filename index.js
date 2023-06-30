60bd5698569537f947cb6d9a77dece3d2b6b7cf7
levymc <levymcruz@gmail.com>
2023-06-30 20:31:38 -0300

    tentando deploy 7

---
+++ index.js
@@ -0,0 +1,22 @@
+import express from 'express';
+import { MongoClient, ObjectId } from 'mongodb';
+import cors from 'cors';
+import dotenv from 'dotenv';
+
+const app = express()
+const PORT = 4000
+
+app.listen(PORT, () => {
+  console.log(`API listening on PORT ${PORT} `)
+})
+
+app.get('/', (req, res) => {
+  res.send('Hey this is my API running 🥳')
+})
+
+app.get('/about', (req, res) => {
+  res.send('This is my about route..... ')
+})
+
+
+export default app
