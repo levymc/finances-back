bd620fd52ffb2a6c764c62828c8f360a2e6dc609
levymc <levymcruz@gmail.com>
2023-06-30 21:24:38 -0300

    tentativa 999

--- index.js
+++ index.js
@@ -1,16 +1,7 @@
 import express from 'express';
-import { MongoClient, ObjectId } from 'mongodb';
-import cors from 'cors';
-import dotenv from 'dotenv';
 
-dotenv.config();
 const app = express();
 const PORT = 4000;
-
-app.use(cors());
-app.use(express.json());
-
-// const mongoClient = new MongoClient(process.env.NEXT_ATLAS_URI);
 
 app.listen(PORT, () => {
   console.log(`API listening on PORT ${PORT} `);
