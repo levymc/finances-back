4e3852b46c5b5ab2f23da69dbf7ee6a709e87f80
levymc <levymcruz@gmail.com>
2023-06-30 21:11:20 -0300

    hide .xonfig

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 // const cors = require('cors');
 // const dotenv = require('dotenv');
 
-dotenv.config();
+// dotenv.config();
 const app = express()
 
 app.use(cors());
